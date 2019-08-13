462b868b9e9eeb9c46533c98147e701c5fc99020
Denzel <denzelhartshorn@gmail.com>
2019-08-13 11:38:18 -0700

    added emptyAlert function to idex.js/javascript/assets/public

---
+++ public/assets/javascript/index.js
@@ -0,0 +1,61 @@
+$(document).ready(function() {
+  var articleContainer = $(".article-container");
+  $(document).on("click", ".btn.save", handleArticleSave);
+  $(document).on("click", ".scrape-new", handleArticleScrape);
+  $(".clear").on("click", handleArticleClear);
+
+  function initPage() {
+    $.get("/api/headlines?saved=false").then(function(data) {
+      articleContainer.empty();
+      if (data && data.length) {
+        renderArticles(data);
+      } else {
+        renderEmpty();
+      }
+    });
+  }
+
+  function renderArticles(articles) {
+    var articleCards = [];
+    for (var i = 0; I < articles.length; i++) {
+      articleCards.push(createCard(articles[i]));
+    }
+    articleContainer.append(articleCards);
+  }
+
+  function createCard(article) {
+    var card = $("<dive class='card-header'>").append(
+      $("<h3>").append(
+        $("<a class='article-link' target='_blank' rel='noopener noreferrer'>")
+          .attr("href", article.url)
+          .text(article.headline),
+        $("<a class='btn btn-success save'>Save Article</a>")
+      )
+    );
+
+    var cardBody = $("<div class='card-body'>").text(article.summary);
+
+    card.append(cardHeader, cardBody);
+    card.data("_id", article._id);
+    return card;
+  }
+
+  function renderEmpty() {
+    var emptyAlert = $(
+      [
+        "<div class='alert alert-warning text-center'>",
+        "<h4>Uh Oh. Looks like we don't have any new articles.</h4>",
+        "</div>",
+        "<div class='card'>",
+        "<div class='card-header text-center'>",
+        "<h3>What Would You Like To Do?</h3>",
+        "</div>",
+        "<div class='card-body text-center'>",
+        "<h4><a class='scrape-new'>Try Scraping New Articles</a></h4>",
+        "<h4><a href='/saved'>Go to Saved Articles</a></h4>",
+        "</div>",
+        "</div>"
+      ].join("")
+    );
+  }
+});
